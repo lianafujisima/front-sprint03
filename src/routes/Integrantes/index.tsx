ea553c8a5e3cd1d525d2034b19a6c23b2e95ee47
victorcho05 <victorhwancho05@gmail.com>
2025-09-28 20:20:27 -0300

    victor 3

--- src/routes/Integrantes/index.tsx
+++ src/routes/Integrantes/index.tsx
@@ -1,6 +1,6 @@
 import FotoLeticia from "../../assets/images/fotoleticia.jpeg";
 import FotoLiana from "../../assets/images/fotoliana.jpeg";
-
+import FotoVictor from "../../assets/images/fotovictor.jpeg";
 
 export default function Integrantes(){
     return(
@@ -12,7 +12,7 @@ export default function Integrantes(){
                     <div>
                         <p>Letícia Santiago e Silva</p>
                         <p>RM 565799</p>
-                        <p>Linkedin: <a href="" className="text-blue-600 hover:underline">Letícia Santiago e Silva</a></p>
+                        <p>Linkedin: <a href="http://linkedin.com/in/leticia-santiago-6b9219354" className="text-blue-600 hover:underline">Letícia Santiago e Silva</a></p>
                         <p>Github: <a href="https://github.com/santiago-leticia" target="_blank">Leticia Santiago e Silva</a></p>
                     </div>
                 </div>
@@ -23,6 +23,15 @@ export default function Integrantes(){
                         <p>RM 565698</p>
                         <p>Linkedin: <a href="https://www.linkedin.com/in/liana-lyumi-morisita-fujisima-22368a367/" target="_blank">Liana Lyumi Morisita Fujisima</a></p>
                         <p>Github: <a href="https://github.com/lianafujisima" target="_blank">Liana Lyumi Morisita Fujisima</a></p>
+                    </div>
+                </div>
+                <div>
+                    <img src={FotoVictor} alt="Foto Victor Cho"/>
+                    <div>
+                        <p>Victor Willian Hwan Cho</p>
+                        <p>RM 565382</p>
+                        <p>Linkedin: <a href="https://www.linkedin.com/in/victor-cho-91a508367?trk=contact-info" target="_blank">Victor Willian Hwan Cho</a></p>
+                        <p>Github: <a href="https://github.com/Victorcho05" target="_blank">Victor Willian Hwan Cho</a></p>
                     </div>
                 </div>
             </section>
