3fb5ca49d9f1eaa983f3bab48c0b5b6a80c9d21e
victorcho05 <victorhwancho05@gmail.com>
2025-09-28 20:53:19 -0300

    victor 4

--- src/routes/Integrantes/index.tsx
+++ src/routes/Integrantes/index.tsx
@@ -7,13 +7,13 @@ export default function Integrantes(){
         <main className="min-h-screen bg-gray-50 py-12 px-6">
             <h1 className="text-3xl font-bold text-center text-gray-800 mb-10">Integrantes</h1>
             <section className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 max-w-6xl mx-auto">
-                <div>
-                    <img src={FotoLeticia} alt="Foto Leticia Santiago"/>
-                    <div>
-                        <p>Letícia Santiago e Silva</p>
-                        <p>RM 565799</p>
-                        <p>Linkedin: <a href="http://linkedin.com/in/leticia-santiago-6b9219354" className="text-blue-600 hover:underline">Letícia Santiago e Silva</a></p>
-                        <p>Github: <a href="https://github.com/santiago-leticia" target="_blank">Leticia Santiago e Silva</a></p>
+                <div  className="bg-white rounded-2xl shadow-md p-6 flex flex-col items-center hover:shadow-xl transition">
+                    <img src={FotoLeticia} alt="Foto Leticia Santiago" className="w-32 h-32 rounded-full object-cover border-4 border-green-500 mb-4"/>
+                    <div className="text-center space-y-1">
+                        <p className="text-lg font-semibold text-gray-800">Letícia Santiago e Silva</p>
+                        <p className="text-gray-600">RM 565799</p>
+                        <p className="text-gray-700">Linkedin: <a href="" className="text-blue-600 hover:underline">Letícia Santiago e Silva</a></p>
+                        <p className="text-gray-700">Github: <a href="https://github.com/santiago-leticia" target="_blank" className="text-blue-600 hover:underline">Leticia Santiago e Silva</a></p>
                     </div>
                 </div>
                 <div>
