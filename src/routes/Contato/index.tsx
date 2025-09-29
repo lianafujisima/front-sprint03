62fc46238c9411bb6bbe20631de6fb3feafdc1d1
santiago-leticia <santiagoleticia68@gmail.com>
2025-09-29 07:42:25 -0300

    leticia commit 5

--- src/routes/Contato/index.tsx
+++ src/routes/Contato/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import WhatsAppIcone from '../../assets/images/whatsapp.png';
 import Telefone from "../../assets/images/telefone.png";
 import Email from "../../assets/images/email.png";
@@ -8,9 +9,13 @@ export default function Contato(){
             <main className="min-h-screen bg-gray-50 py-12 px-6">
             <h1 className="text-emerald-800 text-5xl text-center font-bold mb-8">CONTATO</h1>
             <section className="mb-8">
-                <a href="https://wa.me/5511996534795" target="_blank" className="flex items-center gap-4 bg-green-500 text-white font-semibold px-6 py-4 rounded-xl shadow-lg hover:bg-green-600 transition">
-                <img src={WhatsAppIcone} alt="WhatsApp" className="w-10 h-10"/>
-                    Clique aqui para WhatsApp ou pelo número: <strong>(99) 91111-1111</strong> </a>
+                <p className="text-gray-700 mb-4">Fale com a equipe no WhatsApp clicando no link ou pelo número: <strong>(99) 91111-1111</strong> </p>
+                <Link 
+                to="/agendamento" 
+                className="inline-block bg-green-500 text-white font-semibold px-6 py-3 rounded-xl shadow-lg hover:bg-green-600 transition"
+                >
+                <img src={WhatsAppIcone} alt="WhatsApp" className="w-15 h-15 bg-center"/>
+                </Link>
             </section>
 
             <section  className="mb-8 space-y-4">
