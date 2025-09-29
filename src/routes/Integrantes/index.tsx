849c7aee7e024e01e14c2bc61d02533277ce44dc
victorcho05 <victorhwancho05@gmail.com>
2025-09-28 21:32:35 -0300

    victor 5

--- src/routes/Integrantes/index.tsx
+++ src/routes/Integrantes/index.tsx
@@ -16,25 +16,24 @@ export default function Integrantes(){
                         <p className="text-gray-700">Github: <a href="https://github.com/santiago-leticia" target="_blank" className="text-blue-600 hover:underline">Leticia Santiago e Silva</a></p>
                     </div>
                 </div>
-                <div>
-                    <img src={FotoLiana} alt="Foto Liana Fujisima"/>
-                    <div>
-                        <p>Liana Lyumi Morisita Fujisima</p>
-                        <p>RM 565698</p>
-                        <p>Linkedin: <a href="https://www.linkedin.com/in/liana-lyumi-morisita-fujisima-22368a367/" target="_blank">Liana Lyumi Morisita Fujisima</a></p>
-                        <p>Github: <a href="https://github.com/lianafujisima" target="_blank">Liana Lyumi Morisita Fujisima</a></p>
+                <div  className="bg-white rounded-2xl shadow-md p-6 flex flex-col items-center hover:shadow-xl transition">
+                    <img src={FotoLiana} alt="Foto Liana Fujisima" className="w-32 h-32 rounded-full object-cover border-4 border-green-500 mb-4"/>
+                    <div className="text-center space-y-1">
+                        <p className="text-lg font-semibold text-gray-800">Liana Lyumi Morisita Fujisima</p>
+                        <p className="text-gray-600">RM 565698</p>
+                        <p className="text-gray-700">Linkedin: <a href="https://www.linkedin.com/in/liana-lyumi-morisita-fujisima-22368a367/" target="_blank" className="text-blue-600 hover:underline">Liana Lyumi Morisita Fujisima</a></p>
+                        <p className="text-gray-700">Github: <a href="https://github.com/lianafujisima" target="_blank">Liana Lyumi Morisita Fujisima</a></p>
                     </div>
                 </div>
-                <div>
-                    <img src={FotoVictor} alt="Foto Victor Cho"/>
-                    <div>
-                        <p>Victor Willian Hwan Cho</p>
-                        <p>RM 565382</p>
-                        <p>Linkedin: <a href="https://www.linkedin.com/in/victor-cho-91a508367?trk=contact-info" target="_blank">Victor Willian Hwan Cho</a></p>
-                        <p>Github: <a href="https://github.com/Victorcho05" target="_blank">Victor Willian Hwan Cho</a></p>
+                <div  className="bg-white rounded-2xl shadow-md p-6 flex flex-col items-center hover:shadow-xl transition">
+                    <img src={FotoVictor} alt="Foto Victor Cho" className="w-32 h-32 rounded-full object-cover border-4 border-green-500 mb-4"/>
+                    <div className="text-center space-y-1">
+                        <p className="text-lg font-semibold text-gray-800">Victor Willian Hwan Cho</p>
+                        <p className="text-gray-600">RM 565382</p>
+                        <p className="text-gray-700">Linkedin: <a href="https://www.linkedin.com/in/victor-cho-91a508367?trk=contact-info" target="_blank" className="text-blue-600 hover:underline">Victor Willian Hwan Cho</a></p>
+                        <p className="text-gray-700">Github: <a href="https://github.com/Victorcho05">Victor Willian Hwan Cho</a></p>
                     </div>
                 </div>
             </section>
         </main>
-    )
-}
+    )}
